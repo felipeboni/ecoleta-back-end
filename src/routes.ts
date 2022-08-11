62a419b6bbe6a598cfa3ef323333f49f5d43e27c
Felipe Boni <felipeboni@4x4brasil.com.br>
2022-08-11 08:26:19 -0300

    feat: listing & adding points, listing items

---
+++ src/routes.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create); // create: criar
+routes.get('/points/', pointsController.index); // index: mostrar todos
+routes.get('/points/:id', pointsController.show); // show: mostrar um
+
+
+export default routes;
